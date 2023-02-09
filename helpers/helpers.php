f5bfbb753fcb8fa5f2c512c7b22f520f18773860
nguyenanhung <info@nguyenanhung.com>
2023-02-09 23:08:20 +0700

    Release version 1.0.0

--- helpers/helpers.php
+++ helpers/helpers.php
@@ -1,1 +1,9 @@
 <?php
+if (!function_exists('build_favicon_html_tags')) {
+    function build_favicon_html_tags($baseUrl = '')
+    {
+        $fav = new nguyenanhung\Libraries\Favicon\Favicon();
+
+        return $fav->faviconHtml($baseUrl);
+    }
+}
